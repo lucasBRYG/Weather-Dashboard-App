9b2732dd85403c5c9ee17b610fd05b1a39a35369
lucasBRYG <adio.lucas@gmail.com>
2020-10-30 20:53:57 -0400

    cards now generating, generating readable date from UNIX timestamp

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ $(document).ready(function(){
   if(!localStorage.getItem("return")){
     localStorage.setItem("user-history", JSON.stringify(historyArray));
     localStorage.setItem("return", "true");
-    //localStorage.setItem("forcast-object", JSON.stringify(weatherforcastObject));
   }
 
   renderPastSearches();
@@ -26,7 +25,7 @@ $(document).ready(function(){
       url: weaatherURL,
       method: "GET"
     }).then(function(response) {
-      console.log(response)
+      // console.log(response)
       $("#city-name").text(response.name);
       let lat = response.coord.lat;
       let long = response.coord.lon;
@@ -42,8 +41,6 @@ $(document).ready(function(){
       });
 
     });
-  
-    
 
     historyArray = JSON.parse(localStorage.getItem("user-history"));
     historyArray.unshift(nextSearch);
@@ -58,8 +55,10 @@ function renderPastSearches (){
 
   $(userHistoryDiv).empty();
   historyArray = JSON.parse(localStorage.getItem("user-history"));
-  for (var i = 0; i < historyArray.length; i++){
-    $(userHistoryDiv).append("<tr id = 'search-history-" + i + "><th scope = 'row'></th><td>" + historyArray[i] + "</td></tr>");
+  for (var i = 0; i < 15; i++){
+    if(historyArray[i]){
+      $(userHistoryDiv).append("<tr id = 'search-history-" + i + "><th scope = 'row'></th><td>" + historyArray[i] + "</td></tr>");
+    }
   }
 
 }
@@ -69,16 +68,25 @@ function renderPastSearches (){
 function renderWeather(response) {
   
   //$("#weather-icon").att("source", .data.weather.weathericonURL);
-  $("#temp-data").append(response.current.temp);
-  $("#hum-data").append(response.current.humidity);
-  $("#wind-data").append(response.current.wind_speed);
+  $("#temp-data").append("Current Temperature : " + response.current.temp);
+  $("#hum-data").append("Humidity : " + response.current.humidity);
+  $("#wind-data").append("Wind Speed : " + response.current.wind_speed);
   console.log(response.current.wind_speed)
-  $("#uvi-data").append(response.current.uvi);
+  $("#uvi-data").append("UV Index : " + response.current.uvi);
   console.log(response.current.uvi)
 
   for (let i = 0; i < 5; i++){
-    $(#"five-day-cards").append('<div class="col-md-2dot4"><div id="five-day-1" class="card text-white mb-1"><div class="card-body"><h6>' + response.forcast[i].date + '</h6><img source = ' + weatherObject.forcast[i].data.weathericonURL + ' class="weather-icon" height="auto" width="auto"><p class="temp-5 small">' + weatherObject.forcast[i].data.temp + '</p><p class="hum-5 small">' + weatherObject.forcast[i].data.humidity + '</p></div></div></div>');
+    let todayWeather = response.daily[i];
+    let date = getDate(todayWeather.dt);
+    $("#five-day-cards").append('<div class="col-md-2dot4"><div id="five-day-1" class="card mb-1"><div class="card-body"><h6>' + date + '</h6><img src = http://openweathermap.org/img/wn/' + todayWeather.weather[0].icon + '@2x.png class="weather-icon" height="auto" width="auto" alt = "Weather Icon"><p class="temp-5 small">Expected Temperature : ' + todayWeather.temp.day + '</p><p class="hum-5 small">Expected Humidity : ' + todayWeather.humidity + '</p></div></div></div>');
   }
+}
+
+function getDate(unix) {
+  millSecs = unix * 1000;
+  dateOb = new Date(millSecs);
+  let date = dateOb.toLocaleString("en-US", {month: "long"}) + " " + dateOb.getDate();
+  return date;
 }
 
 // https://www.trilogyed.com/about/careers/
